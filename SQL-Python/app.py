d14daec2222796e41efe9f162225f7ee08af736c
Amogh-007-Rin <amoghdath233@gmail.com>
2025-10-25 21:32:53 +0100

    SQL-Chapter-02

--- SQL-Python/app.py
+++ SQL-Python/app.py
@@ -1,15 +1,20 @@
-from flask import Flask , render_template , Response, Request , globals
+from ast import main
+from flask import Flask , render_template , Response, request , globals
 
 app = Flask(__name__)
 
-app.route("/", methods = ["GET", "POST"])
+app.route("/")
 def home() -> Response:
     return "You are currently on home page"
 
-app.route("/dashboard/student/information", methods = ["GET"])
+app.route("/dashboard/student/information")
 def student_information() -> Response:
     return "This should display the student information"
 
-app.route("/dashboard/teacher/information", methods = ["GET"])
+app.route("/dashboard/teacher/information")
 def teacher_information() -> Response:
     return "This should display the teacher information"
+
+if __name__ == '__main__' :
+
+    app.run(debug=True, host='0.0.0.0', port=5000)
